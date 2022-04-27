d73a3a82e2c6634134dc5f964420116bf3c75e2d
EricksonDutra <ericksond10@gmail.com>
2022-04-26 21:21:31 -0400

    messages errors

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -20,6 +20,8 @@ class _TodoListPageState extends State<TodoListPage> {
   Todo? deletedTodo;
   int? deletedTodoPos;
 
+  String? errorText;
+
   @override
   void initState() {
     super.initState();
@@ -49,19 +51,37 @@ class _TodoListPageState extends State<TodoListPage> {
                         decoration: InputDecoration(
                             border: OutlineInputBorder(),
                             labelText: 'Adicione uma tarefa',
-                            hintText: 'Ex. Estudar...'),
+                            hintText: 'Ex. Estudar...',
+                            errorText: errorText,
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                              color: Color(0xff00d7f3),
+                              width: 2,
+                            )),
+                            labelStyle: TextStyle(
+                              color: Color(0xff00d7f3),
+                            )),
                       ),
                     ),
                     SizedBox(width: 8),
                     ElevatedButton(
                       onPressed: () {
                         String text = todoController.text;
+
+                        if (text.isEmpty) {
+                          setState(() {
+                            errorText = 'O título não pode ser vazio!';
+                          });
+                          return;
+                        }
+
                         setState(() {
                           Todo newTodo = Todo(
                             title: text,
                             dateTime: DateTime.now(),
                           );
                           todos.add(newTodo);
+                          errorText = null;
                         });
                         todoController.clear();
                         todoRepository.saveTodoList(todos);
